5e80fb5f8373e59e2e0a86cda3c8f09d249500f4
unknown <joonastiitson@gmail.com>
2022-12-04 18:10:15 +0200

    lisasin lause seletuseks electricali javascripti

--- Amongus-Webpage/js_electrical.js
+++ Amongus-Webpage/js_electrical.js
@@ -3,4 +3,4 @@ function show(){
             .style.display = "block";
     document.getElementById("lightswitch")
             .style.display="none";
-}
+} /*muudab css-s omadusi nupul ja electricali pildil */
